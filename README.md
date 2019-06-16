c7b141b7f6f7d1c1d73b85da9ae6468a5a1a7110
yiekue <yiekue@qq.com>
2019-06-16 16:34:47 +0800

    基本功能:发请求、帮助信息、指定请求头

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
 # gh2c
-go语言实现的建议http2客户端拨测工具
+go语言实现的http2拨测工具
+
+## 获取
+```
+git clone https://github.com/yiekue/gh2c.git
+```
+
+## 编译
+```
+cd gh2c
+go build -o gh2c gh2c.go
+```
+
+## 使用
+
+查看帮助信息
+```
+*[master][~/code/gh2c]$ ./gh2c -help            
+Usage: ./gh2c -[flags] url
+  -H string
+        custom headers
+  -HKVsep string
+        used for split a custom header key and value (default ":")
+  -Hsep string
+        used for split custom headers (default ";")
+  -body
+        output response body
+  -debug
+        print debug info
+  -help
+        print help info
+  -host string
+        custom Host to override default (default "defaltHost")
+  -method string
+        http method, GET/POST... (default "GET")
+  -v int
+        http version 1/2 (default 2)
+  -verifyCert
+        enable verification of the server certificate
+```
+
+使用http2发起请求，默认只会打印头信息：
+```
+*[master][~/code/gh2c]$ go run gh2c.go -v 2 -H "test:testheadker|test2:testheader2" -Hsep "|" https://example.com/
+< GET HTTP/2.0 /
+< Host: www.chinacache.com
+< Test: testheadker
+< Test2: testheader2
+< User-Agent: GH2C
+<
+> HTTP/2.0 200 OK
+> Etag: W/"5cdbdbc8-2dc0"
+> Last-Modified: Wed, 15 May 2019 09:28:40 GMT
+> Date: Sun, 16 Jun 2019 04:39:54 GMT
+> Server: nginx
+> Cc_cache: TCP_HIT
+> Accept-Ranges: bytes
+> Age: 11967
+> Expires: Mon, 17 Jun 2019 04:39:54 GMT
+> Powered-By-Chinacache: HIT from CMN-CD-b-3g3
+> Content-Type: text/html
+<
+```
+自定义host替代默认的host
+```
+*[master][~/code/gh2c]$ go run gh2c.go -v 2 -H "test:testheadker|test2:testheader2" -host test.com -Hsep "|" https://example.com/
+< GET HTTP/2.0 /
+< Host: test.com
+< User-Agent: GH2C
+< Test: testheadker
+< Test2: testheader2
+<
+> HTTP/2.0 403 Forbidden
+> Content-Type: text/html
+> Content-Length: 162
+> Server: nginx
+> Date: Sun, 16 Jun 2019 08:12:28 GMT
+<
+```
+使用http 1.1发送请求，默认是使用http2：
+```
+*[master][~/code/gh2c]$ go run gh2c.go -v 1 -H "test:testheadker|test2:testheader2" -Hsep "|" https://example.com/ 
+< GET HTTP/1.1 /
+< Host: www.chinacache.com
+< User-Agent: GH2C
+< Test: testheadker
+< Test2: testheader2
+<
+> HTTP/1.1 200 OK
+> Content-Type: text/html
+> Connection: keep-alive
+> Age: 13022
+> Server: nginx
+> Etag: W/"5cdbdbc8-2dc0"
+> Last-Modified: Wed, 15 May 2019 09:28:40 GMT
+> Date: Sun, 16 Jun 2019 04:39:54 GMT
+> Expires: Mon, 17 Jun 2019 04:39:54 GMT
+> Powered-By-Chinacache: HIT from CMN-CD-b-3g3
+> Cc_cache: TCP_HIT
+> Accept-Ranges: bytes
+<
+```
---
+++ gh2c.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"golang.org/x/net/http2"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var help = flag.Bool("help", false, "print help info")
+
+var version = flag.Int("v", 2, "http version 1/2")
+var method = flag.String("method", "GET", "http method, GET/POST...")
+var header = flag.String("H", "", "custom headers")
+var headerSep = flag.String("Hsep", ";", "used for split custom headers")
+var headerKVSep = flag.String("HKVsep", ":", "used for split a custom header key and value")
+var customHost = flag.String("host", "defaltHost", "custom Host to override default")
+
+var verifyCert = flag.Bool("verifyCert", false, "enable verification of the server certificate")
+
+// var sni = flag.String("sni", "", "tls sni")
+
+var outbody = flag.Bool("body", false, "output response body")
+
+// var write = flag.String("w", "gh2c.out", "output response body to file instead of stdout")
+
+var debug = flag.Bool("debug", false, "print debug info")
+
+func usageAndExit() {
+	fmt.Println("Usage:", os.Args[0], "-[flags] url")
+	flag.PrintDefaults()
+	os.Exit(1)
+}
+
+func printHelp() {
+	fmt.Println("Usage:", os.Args[0], "-[flags] url")
+	flag.PrintDefaults()
+	os.Exit(0)
+}
+
+func printReqInfo(req *http.Request) {
+
+	// TODO: req.Proto总是HTTP/1.1
+	switch *version {
+	case 1:
+		fmt.Println("<", req.Method, "HTTP/1.1", req.URL.RequestURI())
+	case 2:
+		fmt.Println("<", req.Method, "HTTP/2.0", req.URL.RequestURI())
+	}
+
+	fmt.Println("< Host:", req.Host)
+	for k, vs := range req.Header {
+		for _, v := range vs {
+			fmt.Printf("< %s: %s\n", k, v)
+		}
+	}
+	fmt.Println("<")
+
+}
+
+func printRespInfo(resp *http.Response) {
+	fmt.Println(">", resp.Proto, resp.Status)
+	for k, vs := range resp.Header {
+		for _, v := range vs {
+			fmt.Printf("> %s: %s\n", k, v)
+		}
+	}
+	fmt.Println("<")
+}
+
+func main() {
+	flag.Parse()
+	if *help {
+		printHelp()
+	}
+
+	url := flag.Arg(0)
+
+	if "" == url {
+		fmt.Println("error: please input URL")
+		usageAndExit()
+	}
+
+	client := &http.Client{}
+
+	// 设置是否需要检验服务端证书，默认不校验
+	tlsConfig := new(tls.Config)
+	if *verifyCert {
+		tlsConfig.InsecureSkipVerify = true
+	} else {
+		tlsConfig.InsecureSkipVerify = false
+		if *debug {
+			fmt.Printf("debug: do not verify cert.\n")
+		}
+	}
+	// if "" != *sni {
+	// 	tlsConfig.ServerName = *sni
+	// }
+
+	// 切换http版本，默认使用http2,
+	switch *version {
+	case 1:
+		client.Transport = &http.Transport{
+			TLSClientConfig: tlsConfig,
+		}
+	case 2:
+		client.Transport = &http2.Transport{
+			TLSClientConfig: tlsConfig,
+		}
+	}
+
+	req, err := http.NewRequest(*method, url, nil)
+	if err != nil {
+		fmt.Println("error: failed to create request,", err)
+		usageAndExit()
+	}
+	// 设置User-Agent
+	req.Header.Set("User-Agent", "GH2C")
+
+	// 判断是否需要覆盖默认的host头域
+	if *customHost != "defaltHost" {
+		req.Host = *customHost
+	}
+
+	// 解析并设置定制的头域
+	if "" != *header {
+		if *debug {
+			fmt.Printf("debug: begin parse custom header: %s\n", *header)
+		}
+		headers := strings.Split(*header, *headerSep)
+		for _, h := range headers {
+			kv := strings.Split(h, *headerKVSep)
+			if kv == nil || len(kv) != 2 {
+				fmt.Println("error: custom heaker parse failed!")
+				usageAndExit()
+			}
+			if *debug {
+				fmt.Printf("debug: set header: %s: %s\n", kv[0], kv[1])
+			}
+			req.Header.Set(kv[0], kv[1])
+		}
+	}
+	printReqInfo(req)
+
+	// 发送请求
+	resp, err := client.Do(req)
+	if nil != err {
+		fmt.Println("error: failed to do request,", err)
+		usageAndExit()
+	}
+	defer resp.Body.Close()
+
+	printRespInfo(resp)
+
+	if *outbody {
+		body, err := ioutil.ReadAll(resp.Body)
+		if nil != err {
+			fmt.Println("error: failed to read body.")
+			usageAndExit()
+		}
+		fmt.Println(string(body))
+	}
+}
